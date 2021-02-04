950408a39ac5efa2ad754e17e88208bb504a1bfa
Luccas Piola <luccas.piola@sciensa.com>
2021-02-04 18:06:00 -0300

    feature: dedicating to vack

--- src/server.ts
+++ src/server.ts
@@ -10,5 +10,5 @@ app.use(routes)
 
 app.listen(3333, () => {
   // eslint-disable-next-line no-console
-  console.log('🚀 Server started on port 3333')
+  console.log('Server started on port 3333')
 })
